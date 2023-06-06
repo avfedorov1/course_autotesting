f6a8913489092c08cafbe37dd8004b97ae39d865
av.fedorov1 <av.fedorov1@tensor.ru>
2023-06-06 22:16:14 +0300

    task_9_1.py (refactoring)

--- homework/lection_9/task_9_1.py
+++ homework/lection_9/task_9_1.py
@@ -7,27 +7,20 @@
 f = open('test_file/task1_data.txt', encoding='utf-8')
 list_row = f.readlines()  # Формируем списки строк, по которым будем итерироваться
 # Создаем новый текстовый файл
-file_finish = open('test_file/task1_answer.txt', mode='w', encoding='utf-8')
-try:
+with open('test_file/task1_answer.txt', mode='w', encoding='utf-8') as file_finish:
     # Итерируемся по каждой строчке файла
     for line in list_row:
-        # Заменяем цифры -> удаляем
-        line = line.replace('0', '')
-        line = line.replace('1', '')
-        line = line.replace('2', '')
-        line = line.replace('3', '')
-        line = line.replace('4', '')
-        line = line.replace('5', '')
-        line = line.replace('6', '')
-        line = line.replace('7', '')
-        line = line.replace('8', '')
-        line = line.replace('9', '')
-        # Полученный результат добавляем в файл
-        file_finish.write(line)
-finally:
-    # Закрываем файлы, с которыми работали
-    f.close()
-    file_finish.close()
+        # Вариант №1: Заменяем цифры -> удаляем (слишком много кода)
+        # line = line.replace('0', '').replace('1', '').replace('2', '').replace('3', '').replace('4', '')\
+        #     .replace('5', '').replace('6', '').replace('7', '').replace('8', '').replace('9', '')
+        # Вариант №2: Более оптимизированный
+        for letter in line:
+            if letter.isdigit():
+                continue
+            else:
+                # Полученный результат добавляем в файл
+                file_finish.write(letter)
+
 
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
 
