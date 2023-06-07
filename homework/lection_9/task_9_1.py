55b907f57e1def00835e3bec524d8f78a840f67c
av.fedorov1 <av.fedorov1@tensor.ru>
2023-06-07 15:57:40 +0300

    task_9_1.py (refactoring)

--- homework/lection_9/task_9_1.py
+++ homework/lection_9/task_9_1.py
@@ -6,20 +6,20 @@
 
 with open('test_file/task1_data.txt', encoding='utf-8') as f:
     list_row = f.readlines()  # Формируем списки строк, по которым будем итерироваться
+lst = ''  # Создаем пустую строку, куда будем записывать в цикле все, кроме цифр.
+for line in list_row:  # Итерируемся по каждой строчке файла
+    # Вариант №1: Заменяем цифры -> удаляем (слишком много кода)
+    # line = line.replace('0', '').replace('1', '').replace('2', '').replace('3', '').replace('4', '')\
+    #     .replace('5', '').replace('6', '').replace('7', '').replace('8', '').replace('9', '')
+    # Вариант №2: Более оптимизированный
+    for letter in line:
+        if letter.isdigit():
+            continue
+        else:
+            lst += letter  # Полученный результат добавляем в файл
 # Создаем новый текстовый файл
 with open('test_file/task1_answer.txt', mode='w', encoding='utf-8') as file_finish:
-    # Итерируемся по каждой строчке файла
-    for line in list_row:
-        # Вариант №1: Заменяем цифры -> удаляем (слишком много кода)
-        # line = line.replace('0', '').replace('1', '').replace('2', '').replace('3', '').replace('4', '')\
-        #     .replace('5', '').replace('6', '').replace('7', '').replace('8', '').replace('9', '')
-        # Вариант №2: Более оптимизированный
-        for letter in line:
-            if letter.isdigit():
-                continue
-            else:
-                # Полученный результат добавляем в файл
-                file_finish.write(letter)
+    file_finish.write(lst)
 
 
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
